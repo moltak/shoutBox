2d94083cd286e306c84290b3e8b882004a55ba18
moltak <moltak@gmail.com>
2015-12-30 01:38:44 +0900

    usertest

--- test/userModelTest.js
+++ test/userModelTest.js
@@ -4,10 +4,22 @@ var should = require('should');
 var mongoose = require('mongoose');
 var UserScheme = require('../model/userModel')
 
+mongoose.connect("mongodb://127.0.0.1:27017/test")
+
 describe('model test', function() {
+
+	beforeEach(function(done) {
+		UserScheme.remove({}, function(err) {
+			done();
+		});
+	});
+
 	describe('User model test', function() {
 		it ('Should it can be save', function(done) {
-			UserScheme.find({}, function(err, doc) {
+			var user = new UserScheme({id: 'userId', password: 'password'});
+			user.save(function(err, doc) {
+				should.not.exist(err);
+				// should.doc.length.equal(1);
 				console.log(doc);
 				done();
 			});
