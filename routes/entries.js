446e77af5d5654aa034a9d2ee19e86673b6f1897
moltak <moltak@gmail.com>
2016-01-01 16:48:20 +0900

    add validate middleware

--- routes/entries.js
+++ routes/entries.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 var Entry = require('../lib/entry');
 var EntrySchema = require('../lib/entrySchema');
+var validate = require('../lib/middleware/validate');
 
 router.get('/', function(req, res, next) {
 	Entry.getRange(0, -1, function(err, entries) {
@@ -18,20 +19,24 @@ router.get('/create', function(req, res, next) {
 	res.render('post', {title: 'Post'});
 });
 
-router.post('/', function(req, res, next) {
-	var data = req.body.data;
-	var title = req.body.title;
+router.post('/', 
+	validate.required('title'), 
+	validate.lengthAbove('title', 4),
+	function(req, res, next) {
+		var data = req.body.data;
+		var title = req.body.title;
 
-	var entry = new Entry(new EntrySchema({
-		userName: res.locals.user.id,
-		title: title,
-		data: data
-	}));
+		var entry = new Entry(new EntrySchema({
+			userName: res.locals.user.id,
+			title: title,
+			data: data
+		}));
 
-	entry.save(function(err) {
-		if (err) return next(err);
-		res.redirect('/post');
-	});
-});
+		entry.save(function(err) {
+			if (err) return next(err);
+			res.redirect('/post');
+		});
+	}
+);
 
 module.exports = router;
