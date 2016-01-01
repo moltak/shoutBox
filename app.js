041ab5c13ad536437d46398ade0f8c42879f847e
moltak <moltak@gmail.com>
2016-01-01 12:47:49 +0900

    login logout 구현.

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ var users = require('./routes/users');
 var mongoose = require('mongoose');
 var messages = require('./lib/message');
 var register = require('./routes/register');
+var login = require('./routes/login');
 
 var app = express();
 
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/register', register);
+app.use('/login', login);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/login.js
@@ -0,0 +1,34 @@
+'use strict'
+
+var express = require('express');
+var router = express.Router();
+var User = require('../lib/user');
+var UserSchema = require('../lib/userSchema');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.render('login', {title: 'Login'});
+});
+
+router.post('/', function(req, res, next) {
+	var data = req.data.user;
+	User.authenticate(data.id, data.password, function(err, user) {
+		if (err) return next(err);
+		if (user) {
+			req.session.uid = user.id;
+			res.redirect('/');
+		} else {
+			res.error("Sorry! invalid credentials.");
+			res.redirect('back');
+		}
+	});
+});
+
+router.get('/logout', function(req, res, next) {
+	req.session.destroy(function(err) {
+		if (err) throw err;
+		res.redirect('/');
+	})
+});
+
+module.exports = router;
