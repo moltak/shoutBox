65f01da2a10180e65f24154472bca54fbabe8175
moltak <moltak@gmail.com>
2016-01-01 15:58:10 +0900

    뷰에 데이터 뿌림.

--- routes/entries.js
+++ routes/entries.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
+
 var Entry = require('../lib/entry');
+var EntrySchema = require('../lib/entrySchema');
 
 router.get('/', function(req, res, next) {
 	Entry.getRange(0, -1, function(err, entries) {
@@ -9,6 +11,26 @@ router.get('/', function(req, res, next) {
 			title: 'Entries',
 			entries: entries,
 		});
+	});
+});
+
+router.get('/create', function(req, res, next) {
+	res.render('post', {title: 'Post'});
+});
+
+router.post('/', function(req, res, next) {
+	var data = req.body.data;
+	var title = req.body.title;
+
+	var entry = new Entry(new EntrySchema({
+		userName: res.locals.user.name,
+		title: title,
+		data: data
+	}));
+
+	entry.save(function(err) {
+		if (err) return next(err);
+		res.redirect('/post');
 	});
 });
 
