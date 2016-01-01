cf8062f3a66f1a3240d59174dc307ef2c9971fe3
moltak <moltak@gmail.com>
2016-01-01 12:34:17 +0900

    view 추가, session 포함

--- app.js
+++ app.js
@@ -4,10 +4,13 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var mongoose = require('mongoose');
+var messages = require('./lib/message');
+var register = require('./routes/register');
 
 var app = express();
 
@@ -21,10 +24,19 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+  genid: function(req) {
+    return genuuid() // use UUIDs for session IDs
+  },
+  resave: false,
+  saveUninitialized: true,
+  secret: 'keyboard cat'
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/register', register);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
