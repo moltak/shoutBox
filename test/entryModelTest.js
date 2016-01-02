01e09a45cd5768a51ce868d1e1ab570f420096b9
moltak <moltak@gmail.com>
2016-01-02 12:29:00 +0900

    entry - user, one to many model 생성중

--- test/entryModelTest.js
+++ test/entryModelTest.js
@@ -3,14 +3,18 @@
 var should = require('should');
 var mongoose = require('mongoose');
 var async = require('async');
+
 var Entry = require('../lib/entry');
 var EntrySchema = require('../lib/entrySchema');
+var User = require('../lib/user');
+var UserSchema = require('../lib/userSchema');
 
 describe('Entry model test', function() {
 	before(function(done) {
 		if (mongoose.connection.readyState != 2) {
 			mongoose.connect("mongodb://127.0.0.1:27017/test")
 		}
+		Entry.removeAll();
 		done();
 	});
 
@@ -41,5 +45,32 @@ describe('Entry model test', function() {
 			doc.length.should.equal(1);
 			done();
 		});
+	});
+
+	it('Should user has many entries', function(done) {
+		var user = new User(new UserSchema({id:'user2', password:'password'}));
+		async.waterfall([
+			function(fn) {
+				user.save(function(err, doc) {
+					fn(null);
+				});
+			},
+			function(fn) {
+				User.findOne('user2', function(err, doc) {
+					fn(null, doc);
+				});
+			},
+			function(user, fn) {
+				var entry = new Entry(new EntrySchema(
+					{data:'data', 
+					title:'title', 
+					userName:'userName',
+					user: user}));
+				entry.save(function(err, doc) {
+					if (err) return console.error(err);
+					done();
+				}) 
+			}
+			]);
 	});
 });
